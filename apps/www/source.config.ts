bad36de56bd897f5f8262d42321a255a0bc7a9d9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-04 21:36:44 +0300

    docs: Extend source doc type with links

--- apps/www/source.config.ts
+++ apps/www/source.config.ts
@@ -1,3 +1,15 @@
-import { defineDocs } from 'fumadocs-mdx/config';
+import { defineDocs, frontmatterSchema } from 'fumadocs-mdx/config';
+import z from 'zod';
 
-export const { docs, meta } = defineDocs();
+export const { docs, meta } = defineDocs({
+  docs: {
+    schema: frontmatterSchema.extend({
+      links: z.optional(
+        z.object({
+          doc: z.optional(z.string()),
+          api: z.optional(z.string()),
+        })
+      ),
+    }),
+  },
+});
