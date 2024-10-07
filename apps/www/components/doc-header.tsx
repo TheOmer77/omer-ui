93b036982a7eda1eeee206f34c71d68d9697613f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-07 15:32:40 +0300

    fix(www): Remove unnecessary `cn` in DocHeader

--- apps/www/components/doc-header.tsx
+++ apps/www/components/doc-header.tsx
@@ -7,9 +7,7 @@ type DocHeaderProps = Pick<Doc, 'title' | 'description'>;
 
 export const DocHeader = ({ title, description }: DocHeaderProps) => (
   <div className='space-y-2'>
-    <h1 className={cn('scroll-m-20 text-3xl font-bold tracking-tight')}>
-      {title}
-    </h1>
+    <h1 className='scroll-m-20 text-3xl font-bold tracking-tight'>{title}</h1>
     {description && (
       <p className='text-base text-muted-foreground'>
         <Balancer>{description}</Balancer>
