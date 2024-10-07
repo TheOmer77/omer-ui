60656d7a43ecfddacdd0c3191da054abfde06f28
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-07 18:42:06 +0300

    refactor(www): Update sidebar to use fumadocs page tree

--- apps/www/app/(app)/docs/layout.tsx
+++ apps/www/app/(app)/docs/layout.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from 'react';
 
 import { DocsSidebarNav } from '@/components/sidebar-nav';
-import { docsConfig } from '@/config/docs';
+import { source } from '@/lib/source';
 
 const Layout = ({ children }: { children: ReactNode }) => (
   <div className='border-b'>
     <div className='container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10'>
-      <DocsSidebarNav config={docsConfig} />
+      <DocsSidebarNav pageTree={source.pageTree} />
       {children}
     </div>
   </div>
--- apps/www/components/sidebar-nav.tsx
+++ apps/www/components/sidebar-nav.tsx
@@ -2,91 +2,65 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { SidebarNavItem } from 'types/nav';
 
 import { ScrollArea } from '@/registry/new-york/ui/scroll-area';
+import type { source } from '@/lib/source';
 import { cn } from '@/lib/utils';
-import type { DocsConfig } from '@/config/docs';
 
-type DocsSidebarNavItemsProps = {
-  items: SidebarNavItem[];
-  pathname: string | null;
-};
-
-export const DocsSidebarNavItems = ({
-  items,
-  pathname,
-}: DocsSidebarNavItemsProps) =>
-  items?.length ? (
-    <div className='grid grid-flow-row auto-rows-max text-sm'>
-      {items.map((item, index) =>
-        item.href && !item.disabled ? (
-          <Link
-            key={index}
-            href={item.href}
-            className={cn(
-              'group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline',
-              item.disabled && 'cursor-not-allowed opacity-60',
-              pathname === item.href
-                ? 'font-medium text-foreground'
-                : 'text-muted-foreground'
-            )}
-            target={item.external ? '_blank' : ''}
-            rel={item.external ? 'noreferrer' : ''}
-          >
-            {item.title}
-            {item.label && (
-              <span className='ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline'>
-                {item.label}
-              </span>
-            )}
-          </Link>
-        ) : (
-          <span
-            key={index}
-            className={cn(
-              'flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline',
-              item.disabled && 'cursor-not-allowed opacity-60'
-            )}
-          >
-            {item.title}
-            {item.label && (
-              <span className='ml-2 rounded-md bg-muted px-1.5 py-0.5 text-xs leading-none text-muted-foreground no-underline group-hover:no-underline'>
-                {item.label}
-              </span>
-            )}
-          </span>
-        )
-      )}
-    </div>
-  ) : null;
-
-type DocsSidebarNavProps = { config: DocsConfig };
-
-export const DocsSidebarNav = ({ config }: DocsSidebarNavProps) => {
+type Node = (typeof source.pageTree)['children'][number];
+type DocsSidebarNavItemProps = { item: Node };
+const DocsSidebarNavItem = ({ item }: DocsSidebarNavItemProps) => {
   const pathname = usePathname();
 
-  const items = pathname?.startsWith('/charts')
-    ? config.chartsNav
-    : config.sidebarNav;
+  if (item.type === 'separator')
+    return (
+      <li className='[&:not(:first-of-type)]:mt-4'>
+        <h4 className='mb-1 rounded-md px-2 py-1 text-sm font-semibold'>
+          {item.name}
+        </h4>
+      </li>
+    );
+
+  const href = item.type === 'folder' ? item.index?.url : item.url,
+    external = item.type === 'page' && item.external;
+
+  if (!href)
+    return (
+      <li className='flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline'>
+        {item.name}
+      </li>
+    );
 
   return (
-    <aside className='fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block'>
-      <ScrollArea className='h-full py-6 pr-6 lg:py-8'>
-        {items.length && (
-          <div className='w-full'>
-            {items.map((item, index) => (
-              <div key={index} className={cn('pb-4')}>
-                <h4 className='mb-1 rounded-md px-2 py-1 text-sm font-semibold'>
-                  {item.title}
-                </h4>
-                {item?.items?.length && (
-                  <DocsSidebarNavItems items={item.items} pathname={pathname} />
-                )}
-              </div>
-            ))}
-          </div>
+    <li>
+      <Link
+        href={href}
+        className={cn(
+          'group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline',
+          pathname === href
+            ? 'font-medium text-foreground'
+            : 'text-muted-foreground'
         )}
+        target={external ? '_blank' : ''}
+        rel={external ? 'noreferrer' : ''}
+      >
+        {item.name}
+      </Link>
+    </li>
+  );
+};
+
+type DocsSidebarNavProps = { pageTree: typeof source.pageTree };
+
+export const DocsSidebarNav = ({ pageTree }: DocsSidebarNavProps) => {
+  return (
+    <aside className='fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 text-sm md:sticky md:block'>
+      <ScrollArea className='h-full py-6 pr-6 lg:py-8'>
+        <ul>
+          {pageTree.children.map((item, idx) => (
+            <DocsSidebarNavItem key={idx} item={item} />
+          ))}
+        </ul>
       </ScrollArea>
     </aside>
   );
